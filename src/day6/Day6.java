33895391def67d35f657a3f9c030efe5a889cd4e
Selena Qian <sq22@duke.edu>
2020-12-06 17:47:23 -0500

    day 6 part 2 complete

--- src/day6/Day6.java
+++ src/day6/Day6.java
@@ -3,10 +3,7 @@ package day6;
 import read.SimpleFileReader;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Day6 {
     List<String> data;
@@ -18,16 +15,22 @@ public class Day6 {
 
     public int totalYes() {
         int sum = 0;
-        Set<Character> yes = new HashSet<>();
+        Set<Character> yes = new HashSet<>(Arrays.asList('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+                'q','r','s','t','u','v','w','x','y','z'));
         for (int i = 0; i < data.size(); i++) {
             System.out.println(data.get(i));
-            for (char c : data.get(i).toCharArray()) {
-                yes.add(c);
+            Set<Character> temp = new HashSet<>();
+            if (!data.get(i).equals("")) {
+                for (char c : data.get(i).toCharArray()) {
+                    temp.add(c);
+                }
+                yes.retainAll(temp);
             }
             if (data.get(i).equals("") || i == data.size()-1) {
                 System.out.println(yes);
                 sum+=yes.size();
-                yes = new HashSet<>();
+                yes = new HashSet<>(Arrays.asList('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+                        'q','r','s','t','u','v','w','x','y','z'));
             }
         }
         return sum;
