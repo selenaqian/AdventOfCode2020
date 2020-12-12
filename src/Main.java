9788183df2a10b924e29ece59a88b71668423f2d
Selena Qian <sq22@duke.edu>
2020-12-12 13:55:53 -0500

    day12 part 1 complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import days.Day11Part2;
-import days.Day11;
+import days.Day12;
 
 import java.io.FileNotFoundException;
 
@@ -7,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Day11 temp = new Day11Part2("data/day11/input.txt");
-            System.out.println(temp.occupiedSeats());
+            Day12 temp = new Day12("data/day12/input.txt");
+            temp.move();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
---
+++ src/days/Day12.java
@@ -0,0 +1,49 @@
+package days;
+
+import java.io.FileNotFoundException;
+
+public class Day12 extends Day {
+    char[] instructions;
+    int currentDir = 0;
+    int currentXpos = 0;
+    int currentYpos = 0;
+    int[] xDirections = new int[]{1,0,-1,0};
+    int[] yDirections = new int[]{0,-1,0,1};
+    int[] values;
+
+    public Day12(String filename) throws FileNotFoundException {
+        super(filename);
+        instructions = new char[data.size()];
+        values = new int[data.size()];
+        for (int i = 0; i < data.size(); i++) {
+            String s = data.get(i);
+            instructions[i] = s.charAt(0);
+            values[i] = Integer.parseInt(s.substring(1));
+        }
+    }
+
+    public void move() {
+        for (int i = 0; i < instructions.length; i++) {
+            if (instructions[i] == 'N') move(0, values[i]);
+            if (instructions[i] == 'S') move(0, -values[i]);
+            if (instructions[i] == 'E') move(values[i], 0);
+            if (instructions[i] == 'W') move(-values[i], 0);
+            if (instructions[i] == 'F') move(values[i]*xDirections[currentDir], values[i]*yDirections[currentDir]);
+            else turn(instructions[i], values[i]);
+        }
+        System.out.printf("%d, %d\n", currentXpos, currentYpos);
+    }
+
+    private void move(int x, int y) {
+        currentXpos+=x;
+        currentYpos+=y;
+    }
+
+    private void turn(char direction, int degrees) {
+        int d = (degrees/90)%4;
+        if (direction == 'L') d = -d;
+        currentDir+=d;
+        if (currentDir < 0) currentDir+=4;
+        if (currentDir > 3) currentDir-=4;
+    }
+}
