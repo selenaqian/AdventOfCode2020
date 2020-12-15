ebfad3963c462974337a4d18a6d67ec95af88b73
Selena Qian <sq22@duke.edu>
2020-12-15 01:13:46 -0500

    day 15 part 2 complete - same code, different input number

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             Day15 temp = new Day15("data/day15/input.txt");
-            System.out.println(temp.getNumber(2020));
+            System.out.println(temp.getNumber(30000000));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
