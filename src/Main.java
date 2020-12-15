135a390177b830243fe287beb716eeff9163c71b
Selena Qian <sq22@duke.edu>
2020-12-14 22:32:16 -0500

    part 2 tested, works on small test2 input

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Day14 temp = new Day14("data/day14/input.txt");
-            System.out.println(temp.writeMemory());
+            Day14 temp = new Day14("data/day14/test2.txt");
+            System.out.println(temp.writeMemory2());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
--- src/days/Day14.java
+++ src/days/Day14.java
@@ -1,17 +1,23 @@
 package days;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Day14 extends Day {
     long[] values;
     long oneMask;
     long zeroMask;
+    Map<Long, Long> valueMap; //maps addresses to values
 
     public Day14(String filename) throws FileNotFoundException {
         super(filename);
         oneMask = 0L;
         zeroMask = 0L;
         values = new long[largestAddress()+1];
+        valueMap = new HashMap<>();
     }
 
     public long writeMemory() {
@@ -31,6 +37,51 @@ public class Day14 extends Day {
             sum+=values[i];
         }
         return sum;
+    }
+
+    public long writeMemory2() {
+        String mask = "";
+        for (String s : data) {
+            if (s.contains("mask")) {
+                mask = s.split("= ")[1];
+            }
+            else {
+                String address = s.split("] = ")[0];
+                Long value = Long.parseLong(s.split("] = ")[1]);
+                address = address.replace("mem[", "");
+                address = String.format("%36s", Long.toBinaryString(Long.parseLong(address))).replace(" ", "0");
+                for (int i = 0; i < address.length(); i++) {
+                    if (mask.charAt(i) == '1') {
+                        address = address.substring(0, i) + '1' + address.substring(i+1);
+                    }
+                    else if (mask.charAt(i) == 'X') {
+                        address = address.substring(0, i) + 'X' + address.substring(i+1);
+                    }
+                }
+                List<Long> addresses = findAddresses(address, new ArrayList<>());
+                for (long addr : addresses) {
+                    valueMap.put(addr, value);
+                }
+            }
+        }
+        long sum = 0L;
+        for (long address : valueMap.keySet()) {
+            sum+=valueMap.get(address);
+        }
+        return sum;
+    }
+
+    private List<Long> findAddresses(String address, List<Long> allAddresses) {
+        if (allAddresses == null) allAddresses = new ArrayList<>();
+        if (!address.contains("X")) allAddresses.add(Long.parseLong(address));
+        else {
+            int index = address.indexOf('X');
+            String one = address.substring(0, index) + '1' + address.substring(index+1);
+            String zero = address.substring(0, index) + '0' + address.substring(index+1);
+            findAddresses(one, allAddresses);
+            findAddresses(zero, allAddresses);
+        }
+        return allAddresses;
     }
 
     private int largestAddress() {
