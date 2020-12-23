14e1a83f650de6e389c712395c7cf56707416142
Selena Qian <sq22@duke.edu>
2020-12-23 16:40:19 -0500

    day 23 part 2 complete

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import days.Day23;
 public class Main {
 
     public static void main(String[] args) {
-        Day23 temp = new Day23("219347865");
+        Day23 temp = new Day23("219347865", 9);
         //Day23 temp = new Day23("389125467");
-        temp.playGame(100);
+        temp.playGame(100, 1);
+        Day23 temp2 = new Day23("219347865", 1000000);
+        temp2.playGame(10000000, 2);
     }
 }
--- src/days/Day23.java
+++ src/days/Day23.java
@@ -4,8 +4,8 @@ public class Day23 {
     private int[] values;
     private int current;
 
-    public Day23(String input) {
-        values = new int[input.length()+1];
+    public Day23(String input, int size) {
+        values = new int[size+1];
         current = Integer.parseInt(input.substring(0, 1));
 
         for (int i = 0; i < input.length() - 1; i++) {
@@ -13,21 +13,36 @@ public class Day23 {
             int next = Integer.parseInt(input.substring(i+1, i+2));
             values[index] = next; //so values array maps from value stored in index to next value in list
         }
-        values[Integer.parseInt(input.substring(input.length() - 1))] = Integer.parseInt(input.substring(0, 1));
+
+        for (int i = input.length() + 1; i < size; i++) {
+            values[i] = i+1;
+        }
+
+        if (size == input.length()) values[Integer.parseInt(input.substring(input.length() - 1))] = Integer.parseInt(input.substring(0, 1));
+        else if (size > input.length()) {
+            values[Integer.parseInt(input.substring(input.length() - 1))] = input.length()+1;
+            values[size] = Integer.parseInt(input.substring(0,1));
+        }
     }
 
-    public void playGame(int n) {
+    public void playGame(int n, int ansType) {
         for (int i = 0; i < n; i++) {
             playRound();
         }
 
-        int i = values[1];
-        String answer = "" + values[1];
-        while (i != 1) {
-            answer+=values[i];
-            i=values[i];
+        if (ansType == 1) {
+            int i = values[1];
+            String answer = "" + values[1];
+            while (i != 1) {
+                answer+=values[i];
+                i=values[i];
+            }
+            System.out.println(answer);
         }
-        System.out.println(answer);
+        else {
+            long ans = (long) values[1] * values[values[1]];
+            System.out.println(ans);
+        }
     }
 
     private void playRound() {
