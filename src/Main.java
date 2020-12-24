74a7666b810fa234a63c22600e571427a9fb139a
Selena Qian <sq22@duke.edu>
2020-12-24 01:46:34 -0500

    day 24 part 1 complete - had a number of bugs in the code but should be fixed now

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             Day24 temp = new Day24("data/day24/input.txt");
+            System.out.println(temp.countBlackTiles());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
--- src/days/Day24.java
+++ src/days/Day24.java
@@ -1,52 +1,116 @@
 package days;
 
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Day24 extends Day {
+    Map<Integer, String> tiles;
 
     public Day24(String filename) throws FileNotFoundException {
         super(filename);
+        tiles = new HashMap<>();
 
-        int minX = Integer.MAX_VALUE;
+        /*int minX = Integer.MAX_VALUE;
         int maxX = Integer.MIN_VALUE;
         int minY = Integer.MAX_VALUE;
         int maxY = Integer.MIN_VALUE;
-        int x = 0;
-        int y = 0;
+        int minZ = Integer.MAX_VALUE;
+        int maxZ = Integer.MIN_VALUE;
         String curr = "";
         for (String s : data) {
+            int x = 0;
+            int y = 0;
+            int z = 0;
             for (char c : s.toCharArray()) {
                 curr+=c+"";
                 if (c == 'w' || c == 'e') {
                     //do stuff with x and y
                     if (curr.equals("ne")) {
-                        y--;
+                        x++;
+                        z--;
                     }
                     else if (curr.equals("e")) {
                         x++;
+                        y--;
                     }
                     else if (curr.equals("se")) {
-                        y++;
+                        y--;
+                        z++;
                     }
                     else if (curr.equals("sw")) {
                         x--;
-                        y++;
+                        z++;
                     }
                     else if (curr.equals("w")) {
                         x--;
+                        y++;
                     }
                     else if (curr.equals("nw")) {
-                        x--;
-                        y--;
+                        z--;
+                        y++;
                     }
                     curr = "";
                 }
             }
             if (x < minX) minX = x;
             if (y < minY) minY = y;
+            if (z < minZ) minZ = z;
             if (x > maxX) maxX = x;
             if (y > maxY) maxY = y;
+            if (z > maxZ) maxZ = z;
         }
-        System.out.printf("%d %d %d %d%n", minX, minY, maxX, maxY);
+        System.out.printf("x: (%d, %d), y: (%d, %d), z: (%d, %d)%n", minX, maxX, minY, maxY, minZ, maxZ);
+        */
+        //x: (-14, 14), y: (-14, 14), z: (-14, 14)
+    }
+
+    public int countBlackTiles() {
+        String curr = "";
+        for (String s : data) {
+            int x = 0;
+            int y = 0;
+            int z = 0;
+            for (char c : s.toCharArray()) {
+                curr+=c+"";
+                if (c == 'w' || c == 'e') {
+                    //do stuff with x, y, z
+                    if (curr.equals("ne")) {
+                        x++;
+                        z--;
+                    }
+                    else if (curr.equals("e")) {
+                        x++;
+                        y--;
+                    }
+                    else if (curr.equals("se")) {
+                        y--;
+                        z++;
+                    }
+                    else if (curr.equals("sw")) {
+                        x--;
+                        z++;
+                    }
+                    else if (curr.equals("w")) {
+                        x--;
+                        y++;
+                    }
+                    else if (curr.equals("nw")) {
+                        z--;
+                        y++;
+                    }
+                    curr = "";
+                }
+            }
+            tiles.putIfAbsent(x*10000+y*100+z, "white");
+            if (tiles.get(x*10000+y*100+z).equals("black")) tiles.put(x*10000+y*100+z, "white");
+            else tiles.put(x*10000+y*100+z, "black");
+        }
+
+        int count = 0;
+        for (int i : tiles.keySet()) {
+            if (tiles.get(i).equals("black")) count++;
+        }
+        return count;
     }
 }
